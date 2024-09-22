768e9feeaa9b80530897b0165ab7d8c6dacd3a4d
Kyrylo <kiril201313bigmir.net@gmail.com>
2024-09-22 19:49:23 -0400

    Rendering Lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
+    // <> indicate a fragment
+    // to render a list dynamicaly we wrap the list component in braces {}
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
