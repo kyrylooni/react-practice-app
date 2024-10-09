e82540b73e7239c4df62a862e0cb4cb92705fb48
Kyrylo <kiril201313bigmir.net@gmail.com>
2024-10-09 14:45:49 -0400

    Passing children as a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 // Parent component (Consumer)
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item)
+  // }
 
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+  // return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+  return (
+    <div>
+      <Alert>
+        Hello <span>world!</span> 
+      </Alert>
+    </div>
+  )
 }
 
 export default App;
