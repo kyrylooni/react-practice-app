dc2f6248f892013b11424f909e7b1e1197c1f18b
Kyrylo <kiril201313bigmir.net@gmail.com>
2024-10-09 13:47:33 -0400

    Passing Functions via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+// Parent component (Consumer)
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  return <div><ListGroup items={items} heading="Cities"/></div>;
+
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  // (item: string) => void
+  onSelectItem : (item: string) => void; 
+
 }
 
 
-function ListGroup({items, heading}: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
   
   // type annotation
 
@@ -31,7 +34,10 @@ function ListGroup({items, heading}: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => {setSelectedIndex(index);}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
