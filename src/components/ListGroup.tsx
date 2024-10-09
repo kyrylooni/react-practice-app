0723f98e0c72277b2b124b91d977d749aba62fac
Kyrylo <kiril201313bigmir.net@gmail.com>
2024-10-08 23:59:05 -0400

    Managing State/hooks

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,16 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   // type annotation
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // a variable to keep track of index of selected item
+  
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+
 
   return (
     // <> indicate a fragment
@@ -13,8 +19,16 @@ function ListGroup() {
       <h1>List</h1>
       {items.length === 0 && <p>No item found </p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index);}}
+          >
             {item}
           </li>
         ))}
